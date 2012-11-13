303043d3237ef1d9d21676a066b028c89d764014
NAME <user@domain>
2012-11-13 10:43:00 +0000

    warnings sous mac (différence d'appostrophe)

--- CMakeRep/CTestCustom.cmake
+++ CMakeRep/CTestCustom.cmake
@@ -3,3 +3,5 @@ SET( CTEST_CUSTOM_MAXIMUM_NUMBER_OF_ERRORS 2000)
 
 SET ( CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
   ".*: warning: array subscript has type 'char'.*")
+SET ( CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
+  ".*: warning: array subscript has type ‘char’.*")
