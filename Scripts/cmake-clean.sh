2cdec7950a38a57737027f62ce1640a777ee31d6
NAME <user@domain>
2014-02-25 10:44:20 +0000

    maj des README

--- Scripts/cmake-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# rm all the temporary files created by cmake
-# usage : ./cmake-clean.sh build
-REP=$1
-cd $REP
-make clean
-rm -rf CMakeFiles/ CMakeCache.txt Makefile DartConfiguration.tcl CTestTestfile.cmake cmake_install.cmake Testing CTestCustom.cmake
-cd ..
