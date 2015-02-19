a598ee9c64884374e520b44e6588a723c5bb0d1c
NAME <user@domain>
2015-02-19 17:09:24 +0000

    Correct case sensitive errors in arborescence + add license file and l-gpl terms

---
+++ cmake/config/CTestConfig.cmake
@@ -0,0 +1,35 @@
+## =============================================================================
+##  This file is part of the MMG3D 5 software package for the tetrahedral
+##  mesh modification.
+##  Copyright (c) 2014 Inria / Université de Bordeaux, IMB / UPMC, LJLL.
+##
+##  MMG3D 5 is free software: you can redistribute it and/or modify it
+##  under the terms of the GNU Lesser General Public License as published
+##  by the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  MMG3D 5 is distributed in the hope that it will be useful, but WITHOUT
+##  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+##  License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public
+##  License and of the GNU General Public License along with MMG3D 5 (in
+##  files COPYING.LESSER and COPYING). If not, see
+##  <http://www.gnu.org/licenses/>. Please read their terms carefully and
+##  use this copy of the MMG3D 5 distribution only if you accept them.
+## =============================================================================
+
+SET(CTEST_PROJECT_NAME "Mmg3d5")
+SET(UPDATE_COMMAND "${SVNCOMMAND}")
+
+SET(CTEST_CONTINUOUS_DURATION 1000)
+SET(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY_ONCE TRUE)
+SET(CTEST_CONTINUOUS_MINIMUM_INTERVAL 60)
+SET(CTEST_NIGHTLY_START_TIME "01:00AM")
+
+SET(CTEST_DROP_METHOD "http")
+SET(CTEST_DROP_SITE "localhost:2000")
+#SET(CTEST_DROP_SITE "vulcain.bordeaux.inria.fr")
+SET(CTEST_DROP_LOCATION "/CDash/submit.php?project=Mmg3d5")
+SET(CTEST_DROP_SITE_CDASH TRUE)
---
+++ cmake/config/CTestCustom.cmake
@@ -0,0 +1,31 @@
+## =============================================================================
+##  This file is part of the MMG3D 5 software package for the tetrahedral
+##  mesh modification.
+##  Copyright (c) 2014 Inria / Université de Bordeaux, IMB / UPMC, LJLL.
+##
+##  MMG3D 5 is free software: you can redistribute it and/or modify it
+##  under the terms of the GNU Lesser General Public License as published
+##  by the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  MMG3D 5 is distributed in the hope that it will be useful, but WITHOUT
+##  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+##  License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public
+##  License and of the GNU General Public License along with MMG3D 5 (in
+##  files COPYING.LESSER and COPYING). If not, see
+##  <http://www.gnu.org/licenses/>. Please read their terms carefully and
+##  use this copy of the MMG3D 5 distribution only if you accept them.
+## =============================================================================
+
+SET( CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 5000)
+SET( CTEST_CUSTOM_MAXIMUM_NUMBER_OF_ERRORS 2000)
+
+SET ( CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
+  ".*: warning: array subscript has type 'char'.*")
+SET ( CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
+  ".*: warning: array subscript has type ‘char’.*")
+SET (CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 0 )
+SET (CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE 0 )
