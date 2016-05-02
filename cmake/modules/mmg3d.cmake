64d6d57c3547cd693d10e138e549c43925187006
Algiane <algiane.froehly@inria.fr>
2016-05-02 10:22:40 +0200

    Try to correct the installation of header files in the mmg directory.

--- cmake/modules/mmg3d.cmake
+++ cmake/modules/mmg3d.cmake
@@ -185,12 +185,14 @@ IF ( LIBMMG3D_STATIC OR LIBMMG3D_SHARED )
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     DEPENDS ${MMG3D_SOURCE_DIR}/libmmg3df.h)
 
-  ADD_CUSTOM_TARGET(headers_copy ALL
-    DEPENDS  ${MMG3D_INCLUDE}/libmmg3df.h  ${MMG3D_INCLUDE}/libmmg3d.h
-    ${COMMON_SOURCE_DIR}/libmmgtypesf.h ${COMMON_SOURCE_DIR}/libmmgtypes.h )
-
   # Install header files in project directory
-  #FILE ( INSTALL ${mmg3d_headers} DESTINATION ${CMAKE_SOURCE_DIR}/include/mmg/mmg3d)
+  FILE(INSTALL  ${mmg3d_headers} DESTINATION ${MMG3D_INCLUDE}
+    PATTERN "libmmg*f.h"  EXCLUDE)
+
+  ADD_CUSTOM_TARGET(copy_3d_headers ALL
+    DEPENDS  ${MMG3D_INCLUDE}/libmmg3df.h  ${MMG3D_INCLUDE}/libmmg3d.h
+    ${MMG3D_INCLUDE}/libmmgtypesf.h ${MMG3D_INCLUDE}/libmmgtypes.h )
+
 ENDIF()
 
 ############################################################################
