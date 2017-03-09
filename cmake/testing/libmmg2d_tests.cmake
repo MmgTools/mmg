9772477c4f26a92bf4d3e257d21f7acd15266c54
Algiane <algiane@gmail.com>
2017-03-09 09:09:31 +0100

    Call a macro to create the example for the mmg2d library.

--- cmake/testing/libmmg2d_tests.cmake
+++ cmake/testing/libmmg2d_tests.cmake
@@ -20,7 +20,6 @@
 ##  use this copy of the mmg distribution only if you accept them.
 ## =============================================================================
 
-INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/include)
 
 ###############################################################################
 #####
@@ -28,90 +27,51 @@ INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/include)
 #####
 ###############################################################################
 
-ADD_EXECUTABLE(libmmg2d_example0_a
-  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0/example0_a/main.c ${mmg2d_includes})
-
-ADD_EXECUTABLE(libmmg2d_example0_b
-  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0/example0_b/main.c ${mmg2d_includes})
-
-ADD_EXECUTABLE(libmmg2d_example1
-  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example1/main.c  ${mmg2d_includes})
-
-ADD_EXECUTABLE(libmmg2d_example2
-  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/squareGeneration_example2/main.c  ${mmg2d_includes})
-
- IF ( WIN32 AND ((NOT MINGW) AND USE_SCOTCH) )
-    my_add_link_flags(libmmg2d_example0_a "/SAFESEH:NO")
-    my_add_link_flags(libmmg2d_example0_b "/SAFESEH:NO")
-    my_add_link_flags(libmmg2d_example1 "/SAFESEH:NO")
-    my_add_link_flags(libmmg2d_example2 "/SAFESEH:NO")
- ENDIF ( )
+SET ( MMG2D_LIB_TESTS
+  libmmg2d_example0_a
+  libmmg2d_example0_b
+  libmmg2d_example1
+  libmmg2d_example2
+  )
+SET ( MMG2D_LIB_TESTS_MAIN_PATH
+  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0/example0_a/main.c
+  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0/example0_b/main.c
+  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example1/main.c
+  ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/squareGeneration_example2/main.c
+  )
 
 IF ( LIBMMG2D_STATIC )
-
-  TARGET_LINK_LIBRARIES(libmmg2d_example0_a lib${PROJECT_NAME}2d_a)
-  TARGET_LINK_LIBRARIES(libmmg2d_example0_b lib${PROJECT_NAME}2d_a)
-  TARGET_LINK_LIBRARIES(libmmg2d_example1   lib${PROJECT_NAME}2d_a)
-  TARGET_LINK_LIBRARIES(libmmg2d_example2   lib${PROJECT_NAME}2d_a)
-
-ELSEIF ( LIBMMG2D_SHARED )
-
-  TARGET_LINK_LIBRARIES(libmmg2d_example0_a lib${PROJECT_NAME}2d_so)
-  TARGET_LINK_LIBRARIES(libmmg2d_example0_b lib${PROJECT_NAME}2d_so)
-  TARGET_LINK_LIBRARIES(libmmg2d_example1   lib${PROJECT_NAME}2d_so)
-  TARGET_LINK_LIBRARIES(libmmg2d_example2   lib${PROJECT_NAME}2d_so)
-
+  SET ( lib_name lib${PROJECT_NAME}2d_a )
+ELSE ( )
+  SET ( lib_name lib${PROJECT_NAME}2d_so )
 ELSE ()
   MESSAGE(WARNING "You must activate the compilation of the static or"
     " shared ${PROJECT_NAME} library to compile this tests." )
-ENDIF ()
+ENDIF ( )
 
-INSTALL(TARGETS libmmg2d_example0_a  RUNTIME DESTINATION bin )
-INSTALL(TARGETS libmmg2d_example0_b  RUNTIME DESTINATION bin )
-INSTALL(TARGETS libmmg2d_example1  RUNTIME DESTINATION bin )
-INSTALL(TARGETS libmmg2d_example2  RUNTIME DESTINATION bin )
-
-###############################################################################
-#####
 #####         Fortran Tests
-#####
-###############################################################################
+IF ( CMAKE_Fortran_COMPILER )
+  ENABLE_LANGUAGE ( Fortran )
 
-IF (CMAKE_Fortran_COMPILER)
-  ENABLE_LANGUAGE (Fortran)
+  SET ( MMG2D_LIB_TESTS ${MMG2D_LIB_TESTS}
+    libmmg2d_fortran_a
+    libmmg2d_fortran_b
+    )
 
-  ADD_EXECUTABLE(libmmg2d_fortran_a
-    ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0_fortran/example0_a/main.F90)
-  ADD_DEPENDENCIES(libmmg2d_fortran_a copy_2d_headers)
-
-  ADD_EXECUTABLE(libmmg2d_fortran_b
-    ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0_fortran/example0_b/main.F90)
-  ADD_DEPENDENCIES(libmmg2d_fortran_b copy_2d_headers)
-
-
-  IF ( WIN32 AND ((NOT MINGW) AND USE_SCOTCH) )
-
-    my_add_link_flags(libmmg2d_fortran_a "/SAFESEH:NO")
-    my_add_link_flags(libmmg2d_fortran_b "/SAFESEH:NO")
-
-  ENDIF ( )
-
-  IF ( LIBMMG2D_STATIC )
-
-    TARGET_LINK_LIBRARIES(libmmg2d_fortran_a lib${PROJECT_NAME}2d_a)
-    TARGET_LINK_LIBRARIES(libmmg2d_fortran_b lib${PROJECT_NAME}2d_a)
-
-  ELSEIF ( LIBMMG2D_SHARED )
-
-    TARGET_LINK_LIBRARIES(libmmg2d_fortran_a lib${PROJECT_NAME}2d_so)
-    TARGET_LINK_LIBRARIES(libmmg2d_fortran_b lib${PROJECT_NAME}2d_so)
-
-  ELSE ()
-    MESSAGE(WARNING "You must activate the compilation of the static or"
-      " shared ${PROJECT_NAME} library to compile this tests." )
-  ENDIF ( )
-
-  INSTALL(TARGETS libmmg2d_fortran_a RUNTIME DESTINATION bin )
-  INSTALL(TARGETS libmmg2d_fortran_b RUNTIME DESTINATION bin )
+  SET ( MMG2D_LIB_TESTS_MAIN_PATH ${MMG2D_LIB_TESTS_MAIN_PATH}
+    ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0_fortran/example0_a/main.F90
+    ${CMAKE_SOURCE_DIR}/libexamples/mmg2d/adaptation_example0_fortran/example0_b/main.F90
+    )
 
 ENDIF ( CMAKE_Fortran_COMPILER )
+
+LIST(LENGTH MMG2D_LIB_TESTS nbTests_tmp)
+MATH(EXPR nbTests "${nbTests_tmp} - 1")
+
+FOREACH ( test_idx RANGE ${nbTests} )
+  LIST ( GET MMG2D_LIB_TESTS           ${test_idx} test_name )
+  LIST ( GET MMG2D_LIB_TESTS_MAIN_PATH ${test_idx} main_path )
+
+  ADD_LIBRARY_TEST ( ${test_name} ${main_path} copy_2d_headers ${lib_name} )
+
+ENDFOREACH ( )
