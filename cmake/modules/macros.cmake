9110d1477032f948a88c10f1ba093a8285aa8068
Algiane <algiane@gmail.com>
2017-03-09 08:56:43 +0100

    Add new CMake Macro to create library test + macro comments.

--- cmake/modules/macros.cmake
+++ cmake/modules/macros.cmake
@@ -1,3 +1,9 @@
+###############################################################################
+#####
+#####         Generation of a fortran header file
+#####
+###############################################################################
+
 MACRO ( GENERATE_FORTRAN_HEADER
     in_dir in_file out_dir out_file name
     )
@@ -20,7 +26,11 @@ MACRO ( GENERATE_FORTRAN_HEADER
 
 ENDMACRO ( )
 
-
+###############################################################################
+#####
+#####         Copy an automatically generated header file to another place
+#####
+###############################################################################
 MACRO ( COPY_FORTRAN_HEADER
     in_dir in_file out_dir out_file
     file_dependencies
@@ -39,6 +49,12 @@ MACRO ( COPY_FORTRAN_HEADER
 
 ENDMACRO ( )
 
+###############################################################################
+#####
+#####         Add a target postfix depending on the build type
+#####
+###############################################################################
+
 MACRO ( ADD_TARGET_POSTFIX target_name )
   IF ( CMAKE_BUILD_TYPE MATCHES "Debug" )
     # in debug mode we name the executable mmgs_debug
@@ -53,4 +69,25 @@ MACRO ( ADD_TARGET_POSTFIX target_name )
     # in MinSizeRel mode we name the executable mmgs_O3
     SET_TARGET_PROPERTIES(${target_name} PROPERTIES MINSIZEREL_POSTFIX _Os)
   ENDIF ( )
+ENDMACRO ( )
+
+###############################################################################
+#####
+#####         Add a library test
+#####
+###############################################################################
+
+MACRO ( ADD_LIBRARY_TEST target_name main_path target_dependencies lib_name )
+  ADD_EXECUTABLE ( ${target_name} ${main_path} )
+  ADD_DEPENDENCIES( ${target_name} ${target_dependencies} )
+
+  TARGET_INCLUDE_DIRECTORIES ( ${target_name} PUBLIC ${CMAKE_SOURCE_DIR}/include )
+
+  IF ( WIN32 AND ((NOT MINGW) AND USE_SCOTCH) )
+    MY_ADD_LINK_FLAGS ( ${target_name} "/SAFESEH:NO" )
+  ENDIF ( )
+
+  TARGET_LINK_LIBRARIES ( ${target_name} ${lib_name} )
+  INSTALL(TARGETS ${target_name} RUNTIME DESTINATION bin )
+
 ENDMACRO ( )
