3968ad91322ee066a34f699ab0ffaf5bcd7eabfd
KUHN Fanny <fkuhn@paf.pedago.ipb.fr>
2018-11-05 17:05:53 +0100

    add of octree coordinates and ip

--- src/mmg3d/MOctree_3d.c
+++ src/mmg3d/MOctree_3d.c
@@ -89,7 +89,7 @@ int MMG3D_init_MOctree_s( MMG5_pMesh mesh, MMG5_MOctree_s* q,int ip, int depth,i
 
   q->split_ls  = split_ls;
   q->leaf = 1;
-
+  q->coordoct=[0,0,0];
   return 1;
 }
 
@@ -114,9 +114,46 @@ int  MMG3D_split_MOctree_s ( MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) 
 
   for(int i=0; i<q->nsons; i++)
   {
-    ip = q->blf_ip * 8 + i;
-    MMG3D_init_MOctree_s(mesh, &tabsons[i], ip, q->depth + 1, 2);
+
+    MMG3D_init_MOctree_s(mesh, &tabsons[i], ip, q->depth + 1, 0);
     tabsons[i].father = q;
+    //calculus of octree coordinates and ip
+    if(i==0)
+    {
+      tabsons[i].coordoct=q.coordoct;
+    }
+    else if(i==1)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[1,0,0];
+    }
+    else if(i==2)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[0,0,1];
+    }
+    else if(i==3)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[1,0,1];
+    }
+    else if(i==4)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[0,1,0];
+    }
+    else if(i==5)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[1,1,0];
+    }
+    else if(i==6)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[0,1,1];
+    }
+    else if(i==7)
+    {
+      tabsons[i].coordoct=q.coordoct+(2^(depth_max)/2^(tabsons[i]->depth))*[1,1,1];
+    }
+
+    /*calculus of ip*/
+    tabsons[i]->blf_ip=tabsons[i].coordoct[2]*2^(2*depth_max)+tabsons[i].coordoct[1]*2^(depth_max)+tabsons[i].coordoct[0]+1;
+  
     if(tabsons[i].depth < depth_max)
     {
       tabsons[i].nsons = 8;
