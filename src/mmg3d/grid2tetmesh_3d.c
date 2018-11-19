720ab95d9d209ec8649e5cd119460116109ae5a1
KUHN Fanny <fkuhn@carlsen.pedago.ipb.fr>
2018-11-19 15:51:56 +0100

    coarsen octree

--- src/mmg3d/grid2tetmesh_3d.c
+++ src/mmg3d/grid2tetmesh_3d.c
@@ -120,135 +120,135 @@ int MMG3D_convert_grid2smallOctree(MMG5_pMesh mesh, MMG5_pSol sol) {
  * level of depth of difference (2:1 balancing).
  *
  */
-static inline
-int MMG3D_balance_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
-
-  int x = q->coordoct[0];
-  int y = q->coordoct[1];
-  int z = q->coordoct[2];
-  int depth;
-  int nb_cells;
-  int father_id;
-  MMG5_MOctree_s* p;
-  int neighboors_tab[18][3];
-
-  //CREATION DU TABLEAU DE VOISINS
-  int i,j,k,tab_id;
-  tab_id=0;
-  int neighboor_span = pow(2,depth_max-q->depth);
-  for(k = z-neighboor_span ; k <= z+neighboor_span ; k++)
-  {
-    for(j = y-neighboor_span ; j <= y+neighboor_span ; j++)
-    {
-      for(i = x-neighboor_span ; i <= x+neighboor_span ; i++)
-      {
-        if((i!=j || i!=k || j!=k) && (i==x || j==y || k==z))
-        {
-          neighboors_tab[tab_id][0] = i;
-          neighboors_tab[tab_id][1] = j;
-          neighboors_tab[tab_id][2] = k;
-          tab_id++;
-        }
-      }
-    }
-  }
-
-  int octree_size = pow(2,depth_max);
-
-  for(i = 0 ; i < 18 ; i++)
-  {
-    p = mesh->octree->root;
-
-    x = neighboors_tab[i][0];
-    y = neighboors_tab[i][1];
-    z = neighboors_tab[i][2];
-
-    //VERIFICATION QUE LE VOISIN APPARTIENT BIEN A LA GRILLE
-    if(x >= 0 && x < octree_size && y >= 0 && y < octree_size && z >= 0 && z < octree_size)
-    {
-      depth = 1;
-      while((p->leaf != 1) && (depth <= depth_max))
-      {
-        nb_cells = pow(2,depth_max-depth);
-        if(x < nb_cells)
-        {
-          if(y < nb_cells)
-          {
-            if(z < nb_cells)
-            {
-              father_id = 0;
-            }
-            else
-            {
-              father_id = 2;
-              z -= nb_cells;
-            }
-          }
-          else
-          {
-            if(z < nb_cells)
-            {
-              father_id = 4;
-            }
-            else
-            {
-              father_id = 6;
-              z -= nb_cells;
-            }
-            y -= nb_cells;
-          }
-        }
-        else
-        {
-          if(y < nb_cells)
-          {
-            if(z < nb_cells)
-            {
-              father_id = 1;
-            }
-            else
-            {
-              father_id = 3;
-              z -= nb_cells;
-            }
-          }
-          else
-          {
-            if(z < nb_cells)
-            {
-              father_id = 5;
-            }
-            else
-            {
-              father_id = 7;
-              z -= nb_cells;
-            }
-            y -= nb_cells;
-          }
-          x -= nb_cells;
-        }
-        p = &p->sons[father_id];
-        depth++;
-      }
-      int tmp,tmp_sum;
-      tmp_sum = 0;
-      if(q->depth==p->depth)//si le voisin est à la même profondeur
-      {
-        if(p->leaf != 1)//s'il n'est pas une leaf
-        {
-          for(tmp = 0; tmp < p->nsons; tmp++)
-          {
-            if(p->sons[i].leaf != 1)//et si au moins un de ses enfants n'est pas une leaf
-            {
-              return 0;
-            }
-          }
-        }
-      }
-    }
-  }
-  return 1;
-}
+// static inline
+// int MMG3D_balance_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
+//
+//   int x = q->coordoct[0];
+//   int y = q->coordoct[1];
+//   int z = q->coordoct[2];
+//   int depth;
+//   int nb_cells;
+//   int father_id;
+//   MMG5_MOctree_s* p;
+//   int neighboors_tab[18][3];
+//
+//   //CREATION DU TABLEAU DE VOISINS
+//   int i,j,k,tab_id;
+//   tab_id=0;
+//   int neighboor_span = pow(2,depth_max-q->depth);
+//   for(k = z-neighboor_span ; k <= z+neighboor_span ; k++)
+//   {
+//     for(j = y-neighboor_span ; j <= y+neighboor_span ; j++)
+//     {
+//       for(i = x-neighboor_span ; i <= x+neighboor_span ; i++)
+//       {
+//         if((i!=j || i!=k || j!=k) && (i==x || j==y || k==z))
+//         {
+//           neighboors_tab[tab_id][0] = i;
+//           neighboors_tab[tab_id][1] = j;
+//           neighboors_tab[tab_id][2] = k;
+//           tab_id++;
+//         }
+//       }
+//     }
+//   }
+//
+//   int octree_size = pow(2,depth_max);
+//
+//   for(i = 0 ; i < 18 ; i++)
+//   {
+//     p = mesh->octree->root;
+//
+//     x = neighboors_tab[i][0];
+//     y = neighboors_tab[i][1];
+//     z = neighboors_tab[i][2];
+//
+//     //VERIFICATION QUE LE VOISIN APPARTIENT BIEN A LA GRILLE
+//     if(x >= 0 && x < octree_size && y >= 0 && y < octree_size && z >= 0 && z < octree_size)
+//     {
+//       depth = 1;
+//       while((p->leaf != 1) && (depth <= depth_max))
+//       {
+//         nb_cells = pow(2,depth_max-depth);
+//         if(x < nb_cells)
+//         {
+//           if(y < nb_cells)
+//           {
+//             if(z < nb_cells)
+//             {
+//               father_id = 0;
+//             }
+//             else
+//             {
+//               father_id = 2;
+//               z -= nb_cells;
+//             }
+//           }
+//           else
+//           {
+//             if(z < nb_cells)
+//             {
+//               father_id = 4;
+//             }
+//             else
+//             {
+//               father_id = 6;
+//               z -= nb_cells;
+//             }
+//             y -= nb_cells;
+//           }
+//         }
+//         else
+//         {
+//           if(y < nb_cells)
+//           {
+//             if(z < nb_cells)
+//             {
+//               father_id = 1;
+//             }
+//             else
+//             {
+//               father_id = 3;
+//               z -= nb_cells;
+//             }
+//           }
+//           else
+//           {
+//             if(z < nb_cells)
+//             {
+//               father_id = 5;
+//             }
+//             else
+//             {
+//               father_id = 7;
+//               z -= nb_cells;
+//             }
+//             y -= nb_cells;
+//           }
+//           x -= nb_cells;
+//         }
+//         p = &p->sons[father_id];
+//         depth++;
+//       }
+//       int tmp,tmp_sum;
+//       tmp_sum = 0;
+//       if(q->depth==p->depth)//si le voisin est à la même profondeur
+//       {
+//         if(p->leaf != 1)//s'il n'est pas une leaf
+//         {
+//           for(tmp = 0; tmp < p->nsons; tmp++)
+//           {
+//             if(p->sons[i].leaf != 1)//et si au moins un de ses enfants n'est pas une leaf
+//             {
+//               return 0;
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return 1;
+// }
 
 
 /**
@@ -260,58 +260,67 @@ int MMG3D_balance_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
  * Build the coarse octree from the initial octree.
  *
  */
-static inline
-int MMG3D_build_coarsen_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
-  int tmp;
-  if(q->leaf != 1)
-  {
-    int i,leaf_sum;
-    leaf_sum=0;
-    for(i=0; i<q->nsons; i++)
-    {
-      leaf_sum += q->sons[i].leaf;
-    }
+// static inline
+// int MMG3D_build_coarsen_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
+//   int tmp;
+//   if(q->leaf != 1)
+//   {
+//     int i,leaf_sum;
+//     leaf_sum=0;
+//     for(i=0; i<q->nsons; i++)
+//     {
+//       leaf_sum += q->sons[i].leaf;
+//     }
+//
+//     if(leaf_sum != q->nsons) // si je ne suis pas un  père QUE de leafs (anciennes et nouvelles)
+//     {
+//       for(i=0; i<q->nsons; i++)
+//       {
+//         if(q->sons[i].leaf != 1)//si je ne suis pas une leaf
+//         {
+//           MMG3D_build_coarsen_octree(mesh, &q->sons[i], depth_max);
+//         }
+//       }
+//     }
+//     if(leaf_sum == q->nsons) // si je suis un père de leafs (anciennes et nouvelles)
+//     {
+//       // crée le split_ls du père
+//       tmp=0;
+//       for(i=0; i<q->nsons; i++)
+//       {
+//         if(q->sons[i].split_ls==0)
+//         {
+//           tmp++;
+//         }
+//       }
+//       if(tmp!=q->nsons)
+//       {
+//         q->split_ls=1; // vérifie si au moins un fils possède la LS
+//       }
+//
+//       if(q->split_ls == 0)
+//       {
+//         // if(MMG3D_balance_octree(mesh,q,depth_max))
+//         // {
+//         MMG3D_merge_MOctree_s (q, mesh);
+//         // }
+//       }
+//     }
+//     leaf_sum=0;
+//   }
+//   return 1;
+//}
 
-    if(leaf_sum != q->nsons) // si je ne suis pas un  père QUE de leafs (anciennes et nouvelles)
-    {
-      for(i=0; i<q->nsons; i++)
-      {
-        if(q->sons[i].leaf != 1)//si je ne suis pas une leaf
-        {
-          MMG3D_build_coarsen_octree(mesh, &q->sons[i], depth_max);
-        }
-      }
-    }
-    if(leaf_sum == q->nsons) // si je suis un père de leafs (anciennes et nouvelles)
-    {
-      // crée le split_ls du père
-      tmp=0;
-      for(i=0; i<q->nsons; i++)
-      {
-        if(q->sons[i].split_ls==0)
-        {
-          tmp++;
-        }
-      }
-      if(tmp!=q->nsons)
-      {
-        q->split_ls=1; // vérifie si au moins un fils possède la LS
-      }
 
-      if(q->split_ls == 0)
-      {
-        // if(MMG3D_balance_octree(mesh,q,depth_max))
-        // {
-        MMG3D_merge_MOctree_s (q, mesh);
-        // }
-      }
-    }
-    leaf_sum=0;
-  }
-  return 1;
-}
-
-
+/**
+* \param q pointer toward the MOctree cell
+ *\param depth_max the depth maximum of the octree.
+ *
+ * \return 1 if success, 0 if fail.
+ *
+ * Merge once the small octree if ls=0 is not in the small octree.
+ *
+ */
 static inline
 int MMG3D_build_coarsen_octree_first_time(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max) {
   int i;
@@ -338,15 +347,24 @@ int MMG3D_build_coarsen_octree_first_time(MMG5_pMesh mesh, MMG5_MOctree_s* q, in
   return 1;
 }
 
+/**
+* \param q pointer toward the MOctree cell
+ *\param depth_max the depth maximum of the octree.
+ *\param depth  the depth of the parent that we want to see if we can merge his sons.
+ * \return 1 if success, 0 if fail.
+ *
+ * Merge the sons of the parent whose depth=depth and balance to have only one depth at maximum beteween them.
+ *
+ */
 static inline
-int MMG3D_build_coarsen_octree_fanny(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max, int depth) {
+int MMG3D_build_coarsen_octree(MMG5_pMesh mesh, MMG5_MOctree_s* q, int depth_max, int depth) {
   // descendre à depth_max -2 la premiere fois qu'on appelle cette fonction puis depth_max-3 etc
   int i;
   if (q->depth < depth)
   {
     for (i=0; i< q->nsons; i++)
     {
-      MMG3D_build_coarsen_octree_fanny(mesh, &q->sons[i], depth_max, depth);
+      MMG3D_build_coarsen_octree(mesh, &q->sons[i], depth_max, depth);
     }
   }
   else
@@ -385,7 +403,6 @@ int MMG3D_build_coarsen_octree_fanny(MMG5_pMesh mesh, MMG5_MOctree_s* q, int dep
       int dir;
       for (dir=0; dir<18; dir++)
       {
-        //il y a 18 directions = 18 voisins
         MMG3D_find_Neighbour_of_Bigger_or_Equal_Size(mesh , &q->sons[i], Direction[dir], &Neighbour);
         if(Neighbour->leaf==0)
         {
@@ -431,8 +448,12 @@ int MMG3D_coarsen_octree(MMG5_pMesh mesh, MMG5_pSol sol) {
   max_dim++;
 
   depth_max=log(max_dim)/log(2);
-
-  MMG3D_build_coarsen_octree(mesh, po, depth_max);
+  MMG3D_build_coarsen_octree_first_time(mesh, po, depth_max);
+  int depth;
+  for (depth=depth_max-2; depth>=0; depth --)
+  {
+    MMG3D_build_coarsen_octree(mesh, po, depth_max, depth);
+  }
 
   return 1;
 }
