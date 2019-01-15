7c23104816c2bb8b995d0d29b882eb30799a251c
Juliette Busquet <jbusquet@enseirb-matmeca.fr>
2019-01-15 16:50:04 +0100

    add comments on delone function.

--- src/mmg3d/delaunay_3d.c
+++ src/mmg3d/delaunay_3d.c
@@ -155,20 +155,20 @@ int MMG5_delone(MMG5_pMesh mesh,MMG5_pSol sol,int ip,int *list,int ilist) {
   base = mesh->base;
   /* external faces */
   size = 0;
-  for (k=0; k<ilist; k++) {
-    old  = list[k];
+  for (k=0; k<ilist; k++) { //on parcourt tous les tétras dans la cavité
+    old  = list[k]; //on prend le tétra k dans la liste
     pt1  = &mesh->tetra[old];
     iadr = (old-1)*4 + 1;
-    adja = &mesh->adja[iadr];
+    adja = &mesh->adja[iadr]; // on récupère les adja du tétra considéré
     vois[0]  = adja[0] >> 2;
     vois[1]  = adja[1] >> 2;
     vois[2]  = adja[2] >> 2;
     vois[3]  = adja[3] >> 2;
     for (i=0; i<4; i++) {
       jel = vois[i];
-      if ( (!jel) || mesh->tetra[jel].flag != base ) {
+      if ( (!jel) || mesh->tetra[jel].flag != base ) { //si l'adja n'existe pas ou n'a pas été traité
         for (j=0; j<3; j++) {
-          i1  = MMG5_idir[i][j];
+          i1  = MMG5_idir[i][j]; // idir[i]: vertices of face opposite to vertex i MMG5_idir[4][3] = { {1,2,3}, {0,3,2}, {0,1,3}, {0,2,1} };
           ppt = &mesh->point[ pt1->v[i1] ];
           ppt->tagdel |= MG_NOM;
         }
