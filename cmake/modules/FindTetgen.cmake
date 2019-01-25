d9d839bb36759dd0e4f647b3253f4db2dd4f5cb4
Algiane <algiane.froehly@inria.fr>
2019-01-25 15:52:11 +0100

    Add FindTetgen.cmake.

---
+++ cmake/modules/FindTetgen.cmake
@@ -0,0 +1,47 @@
+## =============================================================================
+##  This file is part of the mmg software package for the tetrahedral
+##  mesh modification.
+##**  Copyright (c) Bx INP/Inria/UBordeaux/UPMC, 2004- .
+##
+##  mmg is free software: you can redistribute it and/or modify it
+##  under the terms of the GNU Lesser General Public License as published
+##  by the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  mmg is distributed in the hope that it will be useful, but WITHOUT
+##  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+##  License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public
+##  License and of the GNU General Public License along with mmg (in
+##  files COPYING.LESSER and COPYING). If not, see
+##  <http://www.gnu.org/licenses/>. Please read their terms carefully and
+##  use this copy of the mmg distribution only if you accept them.
+## =============================================================================
+
+IF ((NOT WIN32) AND (NOT WIN64))
+  SET ( TETGEN_DIR TETGEN_DIR-NOTFOUND )
+  SET ( TETGEN_EXEC TETGEN_EXEC-NOTFOUND )
+ENDIF()
+
+FIND_PATH(TETGEN_DIR
+  NAMES tetgen.h
+  HINTS ${TETGEN_DIR}
+  $ENV{TETGEN_DIR}
+  $ENV{HOME}/tetgen
+  DOC "Directory of TETGEN Header")
+
+# Check for elas library (search in common locations in order to prevent
+# modifications in ELAS installation)
+FIND_PROGRAM(TETGEN_EXEC
+  NAMES tetgen
+  HINTS ${TETGEN_DIR} ${TETGEN_DIR}/build
+  $ENV{TETGEN_DIR}  $ENV{TETGEN_DIR}/build
+  $ENV{HOME}/bin
+  DOC "The tetgen executable"
+  )
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TETGEN DEFAULT_MSG
+  TETGEN_EXEC)
