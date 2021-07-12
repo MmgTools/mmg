a98ab4a0047f9e89b00c4a85524d31446f113948
Algiane <algiane.froehly@inria.fr>
2021-07-12 11:55:01 +0200

    More clear error messages.

--- src/mmg3d/boulep_3d.c
+++ src/mmg3d/boulep_3d.c
@@ -1823,7 +1823,7 @@ int MMG5_coquilface(MMG5_pMesh mesh,int start,int8_t iface,int ia,int *list,
           // 2) we have a non-manifold shape immersed in a domain (3 triangles
           // sharing the edge and a closed shell)
           printf("  ## Warning: %s: you have %d boundaries in the shell"
-                 " of your edge.\n",__func__,nbdy+1);
+                 " of a manifold edge.\n",__func__,nbdy+1);
           printf("  Problem may occur during remesh process.\n");
           mmgWarn0 = 1;
         }
