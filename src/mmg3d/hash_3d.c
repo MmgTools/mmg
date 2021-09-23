MmgTools/mmg

commit b89426d355f8a75804f84a60a1aaac65dec23a34

Avoid tagging as MG_BDY all edges from input triangles (to test).

src/mmg3d/hash_3d.c
@@ -1102,8 +1102,6 @@ int MMG5_hGeom(MMG5_pMesh mesh) {
         if ( mesh->info.nosurf && (tag & MG_REQ) )
           pt->tag[i] &= ~MG_NOSURF;
 
-        /* Mark edges as boundary edges */
-        pt->tag[i] |= (tag | MG_BDY);
         MMG5_hTag(&mesh->htab,pt->v[i1],pt->v[i2],edg,pt->tag[i]);
       }
     }

commit e61653adb1bb334716cc5c3a02c381fd60a4845a

Patch bug introduced by commit b89426d3 : the input edge tags were not stored anymore inside the triangles (so we loose input ridges for example).

src/mmg3d/hash_3d.c
@@ -1116,6 +1116,9 @@ int MMG5_hGeom(MMG5_pMesh mesh) {
         if ( mesh->info.nosurf && (tag & MG_REQ) )
           pt->tag[i] &= ~MG_NOSURF;
 
+        /* Store the edge tag inside the triangle */
+        pt->tag[i] |= tag;
+
         MMG5_hTag(&mesh->htab,pt->v[i1],pt->v[i2],edg,pt->tag[i]);
       }
     }