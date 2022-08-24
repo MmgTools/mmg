297b9a9886a2fc947a78f694e324152d977fb49d
Algiane Froehly <algiane@gmail.com>
2022-08-24 17:02:04 +0200

    New macro to expand prototypes of pointer function in .h/.c files.

--- src/mmgs/mmgsexterns.h
+++ src/mmgs/mmgsexterns.h
@@ -2,13 +2,22 @@
 #define MMGSEXTERNS_H
 
 #include "libmmgtypes.h"
+#include "mmgcommon.h"
 
-extern double (*MMG5_calelt)(MMG5_pMesh mesh,MMG5_pSol met,MMG5_pTria ptt);
-extern int    (*MMGS_defsiz)(MMG5_pMesh mesh,MMG5_pSol met);
-extern int    (*MMGS_gradsiz)(MMG5_pMesh mesh,MMG5_pSol met);
-extern int    (*MMGS_gradsizreq)(MMG5_pMesh mesh,MMG5_pSol met);
-extern int    (*intmet)(MMG5_pMesh mesh,MMG5_pSol met,int k,int8_t i,int ip,double s);
-extern int    (*movintpt)(MMG5_pMesh mesh,MMG5_pSol met,int *list,int ilist);
-extern int    (*movridpt)(MMG5_pMesh mesh,MMG5_pSol met,int *list,int ilist);
+#ifndef MMG_EXTERN
+#define MMG_EXTERN extern
+#define MMG_ASSIGN_NULL
+#endif
+
+FUNCTION_POINTER ( double (*MMG5_calelt)(MMG5_pMesh mesh,MMG5_pSol met,MMG5_pTria ptt) );
+FUNCTION_POINTER ( int    (*MMGS_defsiz)(MMG5_pMesh mesh,MMG5_pSol met) );
+FUNCTION_POINTER ( int    (*MMGS_gradsiz)(MMG5_pMesh mesh,MMG5_pSol met) );
+FUNCTION_POINTER ( int    (*MMGS_gradsizreq)(MMG5_pMesh mesh,MMG5_pSol met) );
+FUNCTION_POINTER ( int    (*intmet)(MMG5_pMesh mesh,MMG5_pSol met,int k,int8_t i,int ip,double s) );
+FUNCTION_POINTER ( int    (*movintpt)(MMG5_pMesh mesh,MMG5_pSol met,int *list,int ilist) );
+FUNCTION_POINTER ( int    (*movridpt)(MMG5_pMesh mesh,MMG5_pSol met,int *list,int ilist) );
+
+#undef MMG_EXTERN
+#undef MMG_ASSIGN_NULL
 
 #endif
