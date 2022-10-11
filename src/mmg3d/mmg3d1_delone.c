36697743d93811d3dadbbfd91a33db0fa8586af3
Algiane Froehly <algiane@gmail.com>
2022-10-11 14:03:52 +0200

    Fix segfault.

--- src/mmg3d/mmg3d1_delone.c
+++ src/mmg3d/mmg3d1_delone.c
@@ -214,7 +214,7 @@ int MMG3D_mmg3d1_delone_split(MMG5_pMesh mesh, MMG5_pSol met,
   MMG5_int      ip1,ip2;
   MMG5_int      src,ip,ref;
   int           ilist;
-  int16_t       tag;
+  int16_t       is_ifa1_bdy;
   int8_t        j,i,i1,i2,ifa0,ifa1;
 
   if ( lmax < MMG3D_LOPTL_DEL )  {
@@ -223,16 +223,22 @@ int MMG3D_mmg3d1_delone_split(MMG5_pMesh mesh, MMG5_pSol met,
   }
 
   /** Edge is too long: try to split it */
-  pt = &mesh->tetra[k];
-  pxt = pt->xt ? &mesh->xtetra[pt->xt] : 0;
-
   /* Try to treat the edge from a bdy face if possible */
   ifa0 = MMG5_ifar[imax][0];
   ifa1 = MMG5_ifar[imax][1];
 
-  int16_t is_ifa1_bdy = (pxt->ftag[ifa1] & MG_BDY);
+  pt = &mesh->tetra[k];
 
-  i  = (pt->xt && is_ifa1_bdy) ? ifa1 : ifa0;
+  if ( pt->xt ) {
+    pxt = &mesh->xtetra[pt->xt];
+    is_ifa1_bdy = (pxt->ftag[ifa1] & MG_BDY);
+  }
+  else {
+    pxt = NULL;
+    is_ifa1_bdy = 0;
+  }
+
+  i  = (pxt && is_ifa1_bdy) ? ifa1 : ifa0;
   j  = MMG5_iarfinv[i][imax];
   i1 = MMG5_idir[i][MMG5_inxt2[j]];
   i2 = MMG5_idir[i][MMG5_iprv2[j]];
@@ -241,11 +247,12 @@ int MMG3D_mmg3d1_delone_split(MMG5_pMesh mesh, MMG5_pSol met,
   p0  = &mesh->point[ip1];
   p1  = &mesh->point[ip2];
 
-  if ( pt->xt && (pxt->ftag[i] & MG_BDY) ) {
+  if ( pxt && (pxt->ftag[i] & MG_BDY) ) {
     /** Edge belongs to a boundary face: try to split using patterns */
     /* Construction of bezier edge */
-    int8_t ier = MMG3D_build_bezierEdge(mesh,k,imax,i,j,pxt,ip1,ip2,p0,p1,
-                                        &ref,&tag,o,to,no1,no2,list,&ilist);
+    int16_t tag;
+    int8_t  ier = MMG3D_build_bezierEdge(mesh,k,imax,i,j,pxt,ip1,ip2,p0,p1,
+                                         &ref,&tag,o,to,no1,no2,list,&ilist);
     switch (ier) {
     case -1:
       /* Strong failure */
