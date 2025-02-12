3bc5373eb6768dd511c95598b0c6a8211dd8b2e6
Corentin Prigent <corentin.prigent@inria.fr>
2025-02-12 17:23:19 +0100

    Development of Python API: definition of meshes and points

--- scripts/genpython.py
+++ scripts/genpython.py
@@ -1,108 +1,19 @@
-import os
+from writeAPI import *
+from mmg_classes import *
 
-class mmgFunction():
-    def __init__(self,name,rtype,args):
-        self.name = name
-        self.return_type = rtype
-        self.args = args
-
-class arg():
-    def __init__(self,arg_name,arg_type):
-        self.name = arg_name
-        self.type = arg_type
-
-class mmgClass():
-    def __init__(self,name,):
-        self.name = name
-        self.args = []
-
-    def addArgs(self,args):
-        for f in args:
-            self.args.append(f)
-
-class pythonAPI:
-
-    def __init__(self, namespace="mmg"):
-        self.ns = namespace
-        self.classes   = []
-        self.functions = []
-
-    def addClass(self,cl):
-        self.classes.append(cl)
-
-    def addFunction(self,fn):
-        self.functions.append(fn)
-
-    def writeAPI(self):
-        def writeClass(f, cl):
-            f.write("class " + cl.name + "(ctypes.Structure):\n")
-            indentcl = "    "
-            indentfl = 4*indentcl
-            f.write(indentcl + "_fields_ = [")
-            for a in cl.args:
-                if  (a == cl.args[0]):
-                    f.write("(\""+a.name+"\","+a.type+"),\n")
-                elif(a == cl.args[-1]):
-                    f.write(indentfl+"(\""+a.name+"\","+a.type+")")
-                else:
-                    f.write(indentfl+"(\""+a.name+"\","+a.type+"),\n")
-            f.write("]\n\n")
-
-        def writeFunctionResArgs(f,fn):
-            f.write("lib." + fn.name + ".argtypes = (")
-            for a in fn.args:
-                f.write("ctypes.POINTER(" + a.type + "),")
-            f.write(")\n")
-            f.write("lib." + fn.name + ".restype = ")
-            f.write(fn.return_type)
-            f.write("\n\n")
-
-        def writeFunction(f, fn):
-            indentfn = "    "
-            f.write("def " + fn.name + "(")
-            for a in fn.args:
-                if (not (a == fn.args[-1])):
-                    f.write(a.name + ": " + a.type + ",")
-                else:
-                    f.write(a.name + ": " + a.type)
-            f.write("):\n")
-            f.write(indentfn + "lib." + fn.name + "(")
-            for a in fn.args:
-                if (not (a == fn.args[-1])):
-                    f.write("ctypes.byref(" + a.name + "),")
-                else:
-                    f.write("ctypes.byref(" + a.name + ")")
-            f.write(")")
-
-
-        with open(self.ns + ".py", "w") as f:
-            f.write(python_header)
-            for cl in self.classes:
-                writeClass(f,cl)
-            for fn in self.functions:
-                writeFunctionResArgs(f,fn)
-            for fn in self.functions:
-                writeFunction(f,fn)
-            
-
-python_header = """import ctypes
-import os
-
-libdir  = os.path.dirname(os.path.realpath(__file__))
-libpath = os.path.join(libdir,"libtest.dylib")
-
-lib = ctypes.CDLL(libpath)
-
-"""
-
-point = mmgClass("Point")
+point_tmp = mmgClass("Point")
 point.addArgs([arg("a","ctypes.c_int"),arg("x","ctypes.c_double"),arg("y","ctypes.c_double")])
 
 printPoint = mmgFunction("printPoint","ctypes.c_int",[arg("p","Point")])
 
 api = pythonAPI()
 
-api.addClass(point)
+api.addClass(point_tmp)
 api.addFunction(printPoint)
+
+api.addClass(point)
+api.addClass(mesh)
+
+
 
 api.writeAPI()
---
+++ scripts/mmg_classes.py
@@ -0,0 +1,63 @@
+from writeAPI import *
+
+MMG5_int = "ctypes.c_int"
+
+# Point
+
+point = mmgClass("MMG5_Point")
+point.addArgs([arg("c","ctypes.c_double * 3")])
+point.addArgs([arg("n","ctypes.c_double * 3")])
+#point.addArgs([arg("src","ctypes.c_double")])
+point.addArgs([arg("ref",MMG5_int)])
+point.addArgs([arg("xp",MMG5_int)])
+point.addArgs([arg("tmp",MMG5_int)])
+point.addArgs([arg("flag",MMG5_int)])
+point.addArgs([arg("s",MMG5_int)])
+point.addArgs([arg("tag","ctypes.c_uint16")])
+point.addArgs([arg("tagdel","ctypes.c_int8")])
+
+MMG5_pPoint = "ctypes.POINTER("+point.name+")"
+
+# mesh
+
+mesh = mmgClass("MMG5_mesh")
+mesh.addArgs([arg("memMax","ctypes.c_size_t")])
+mesh.addArgs([arg("memCur","ctypes.c_size_t")])
+mesh.addArgs([arg("gap","ctypes.c_double")])
+mesh.addArgs([arg("ver","ctypes.c_int")])
+mesh.addArgs([arg("dim","ctypes.c_int")])
+mesh.addArgs([arg("type","ctypes.c_int")])
+mesh.addArgs([arg("npi",MMG5_int),arg("nti",MMG5_int),arg("nai",MMG5_int),arg("nei",MMG5_int)])
+mesh.addArgs([arg("np",MMG5_int),arg("na",MMG5_int),arg("nt",MMG5_int),arg("ne",MMG5_int)])
+mesh.addArgs([arg("npmax",MMG5_int),arg("namax",MMG5_int),arg("ntmax",MMG5_int),arg("nemax",MMG5_int)])
+mesh.addArgs([arg("xpmax",MMG5_int),arg("xtmax",MMG5_int)])
+mesh.addArgs([arg("nquad",MMG5_int),arg("nprism",MMG5_int)])
+mesh.addArgs([arg("nsols","ctypes.c_int")])
+mesh.addArgs([arg("nc1",MMG5_int)])
+mesh.addArgs([arg("base",MMG5_int)])
+mesh.addArgs([arg("mark",MMG5_int)])
+mesh.addArgs([arg("xp",MMG5_int),arg("xt",MMG5_int),arg("xpr",MMG5_int)])
+mesh.addArgs([arg("npnil",MMG5_int)])
+mesh.addArgs([arg("nenil",MMG5_int)])
+mesh.addArgs([arg("nanil",MMG5_int)])
+mesh.addArgs([arg("adja","ctypes.POINTER("+MMG5_int+")")])
+mesh.addArgs([arg("adjt","ctypes.POINTER("+MMG5_int+")")])
+mesh.addArgs([arg("adjapr","ctypes.POINTER("+MMG5_int+")")])
+mesh.addArgs([arg("adjq","ctypes.POINTER("+MMG5_int+")")])
+mesh.addArgs([arg("ipar","ctypes.POINTER(ctypes.c_int)")])
+mesh.addArgs([arg("point",MMG5_pPoint)])
+mesh.addArgs([arg("xpoint","MMG5_pxPoint")])
+mesh.addArgs([arg("tetra","MMG5_pTetra")])
+mesh.addArgs([arg("xtetra","MMG5_pxTetra")])
+mesh.addArgs([arg("prism","MMG5_pPrism")])
+mesh.addArgs([arg("xprism","MMG5_pxPrism")])
+mesh.addArgs([arg("tria","MMG5_pTria")])
+mesh.addArgs([arg("quadra","MMG5_pQuad")])
+mesh.addArgs([arg("edge","MMG5_pEdge")])
+mesh.addArgs([arg("htab","MMG5_HGeom")])
+mesh.addArgs([arg("info","MMG5_Info")])
+mesh.addArgs([arg("namein","ctypes.c_char_p")])
+mesh.addArgs([arg("nameout","ctypes.c_char_p")])
+
+
+
