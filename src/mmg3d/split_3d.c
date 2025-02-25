MmgTools/mmg

commit 51d5bdfd7a9a01355ce4ac8aca4a7c7878ea1c7a

get rid of xt0

src/mmg3d/split_3d.c
@@ -4484,7 +4484,7 @@ int MMG3D_split6_sim(MMG5_pMesh mesh,MMG5_pSol met,MMG5_int k,MMG5_int vx[6]) {
  */
 int MMG5_split6(MMG5_pMesh mesh,MMG5_pSol met,MMG5_int k,MMG5_int vx[6],int8_t metRidTyp) {
   MMG5_pTetra    pt[8];
-  MMG5_xTetra    xt0,yt[8];
+  MMG5_xTetra    yt[8];
   MMG5_pxTetra   pxt;
   int            i,j;
   MMG5_int       iel,newtet[8],nxt0;
@@ -4498,7 +4498,6 @@ int MMG5_split6(MMG5_pMesh mesh,MMG5_pSol met,MMG5_int k,MMG5_int vx[6],int8_t m
 
   nxt0 = pt[0]->xt;
   pxt = &mesh->xtetra[nxt0];
-  memcpy(&xt0,pxt,sizeof(MMG5_xTetra));
 
   /* create 7 new tetras */
   if ( !MMG3D_crea_newTetra(mesh,ne,newtet,pt,yt,&pxt) ) {
@@ -4507,7 +4506,7 @@ int MMG5_split6(MMG5_pMesh mesh,MMG5_pSol met,MMG5_int k,MMG5_int vx[6],int8_t m
 
   /* Store face tags and refs from split tetra*/
   for (i=0; i<4; i++) {
-    ftag[i] = (xt0.ftag[i] & ~MG_REF);
+    ftag[i] = (pxt->ftag[i] & ~MG_REF);
   }
 
   /* Modify first tetra */

commit 72f3a350ea1c60575dedba15e00bdbd9c8681f30

fixed a bug that I introduced in commit 51d5bdfd7a9a01 Note that MMG3D_crea_newTetra() modifies its last argument, pxt, to become a null pointer when pt[0] has no xt. Before it was a pointer to xtetra[0]. So use it first.

src/mmg3d/split_3d.c
@@ -4501,14 +4501,14 @@ int MMG5_split6(MMG5_pMesh mesh,MMG5_pSol met,MMG5_int k,MMG5_int vx[6],int8_t m
   nxt0 = pt[0]->xt;
   pxt0 = &mesh->xtetra[nxt0];
 
+  /* Store face tags and refs from split tetra (before modifying pxt) */
+  for (i=0; i<4; i++) {
+    ftag[i] = (pxt0->ftag[i] & ~MG_REF);
+  }
+
   /* create 7 new tetras and initialize as copies of the original */
   if ( !MMG3D_crea_newTetra(mesh,ne,newtet,pt,yt,&pxt0) ) {
     return 0;
-  }
-
-  /* Store face tags and refs from split tetra*/
-  for (i=0; i<4; i++) {
-    ftag[i] = (pxt0->ftag[i] & ~MG_REF);
   }
 
   /* Modify first tetra */