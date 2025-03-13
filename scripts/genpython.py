3ecfb7f273b47a9c586109d6e62a5304953a38a9
Corentin Prigent <corentin.prigent@inria.fr>
2025-03-13 15:20:38 +0100

    added function setAPIClasses

--- scripts/genpython.py
+++ scripts/genpython.py
@@ -1,12 +1,11 @@
 from writeAPI import *
 from mmg_classes import *
+from mmg_functions import *
 
 api = pythonAPI()
+setAPIClasses(api)
+setAPIFunctions(api)
+api.write_api()
 
 #printPoint = mmgFunction("printPoint","ctypes.c_int",[arg("p","Point")])
 #api.addFunction(printPoint)
-
-for cl in classes:
-    api.addClass(cl)
-
-api.writeAPI()
